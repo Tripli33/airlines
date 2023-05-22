65f07f84997697a612858cd60b475e3701136e29
Roman Savenko <rsaven.wk@gmail.com>
2023-05-22 21:07:39 +0300

    feat: add unit testing custom user details service

--- src/main/java/com/course_project/airlines/services/CustomUserDetailsService.java
+++ src/main/java/com/course_project/airlines/services/CustomUserDetailsService.java
@@ -16,7 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
      * Method that loads the user's data entity by their email address
      * @param email - email address of the current user
      * @return - entity containing the user's details
-     * @throws - UsernameNotFoundException if no user is found with the given email address
+     * @throws UsernameNotFoundException - if no user is found with the given email address
      */
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
