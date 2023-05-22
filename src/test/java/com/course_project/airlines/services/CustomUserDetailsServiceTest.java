a7581be347f5bc7ae1055ee9fea5f10c7048ebd1
Roman Savenko <rsaven.wk@gmail.com>
2023-05-22 22:07:06 +0300

    feat: add .gitattributes

--- src/test/java/com/course_project/airlines/services/CustomUserDetailsServiceTest.java
+++ src/test/java/com/course_project/airlines/services/CustomUserDetailsServiceTest.java
@@ -8,12 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
-import javax.persistence.EntityNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class CustomUserDetailsServiceTest {
